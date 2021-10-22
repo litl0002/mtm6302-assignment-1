82017aadff068b0d139b6334007c4b2adefe307a
Adam Litle <litl0002@algonquinlive.com>
2021-10-22 11:05:09 -0400

    Completes assignment

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-/*
-const $emojis = document.getElementById('emojis')
-$emojis.innerHTML += 
-'&#8986; &#8987; &#9193; &#9194;' 
-*/
-
 let $character = document.getElementById('emojis')
 for (let i=129410; i<=129510; i++) {
-    $character.innerHTML += `<div>
+    $character.innerHTML += `<div> 
                                 <div>&#${i};</div> 
                                 <p>${i}</p>
                             </div>`
